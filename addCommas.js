fe2411c8640ad74936c9ab418e7a84b95c658de8
Joe Rezendes <rezenbiz@gmail.com>
2021-09-01 23:54:11 -0700

    add commas function, passes

--- addCommas.js
+++ addCommas.js
@@ -1,5 +1,22 @@
+const stringReverse = require('./stringReverse');
+
 function addCommas(num) {
-    return;
+    num = `${num}`;
+
+    // go from end of string to begginning of string, and every three characters add a comma. (use % 3)
+    num = stringReverse(num);
+    num = num.split('');
+    
+    for (let i = 0; i < num.length; i++) {
+        if (i - 1 % 3 === 2) {
+            num.splice(i, 0, ',');
+        }
+    }
+    num = num.join('');
+    num = stringReverse(num);
+    return num;
 }
 
 module.exports = addCommas;
+
+addCommas(10000);
--- stringReverse.test.js
+++ stringReverse.test.js
@@ -1,4 +1,4 @@
-stringReverse = require('./stringReverse');
+const stringReverse = require('./stringReverse');
 
 test('adds 1 + 2 to equal 3', () => {
     expect(stringReverse('did this work')).toBe('krow siht did');
